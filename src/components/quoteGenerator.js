76fbb541e2319aa49883aa323b6869ef79598860
alexis <alexisalfaro505@gmail.com>
2025-07-30 16:31:54 -0600

    Create the components folder

---
+++ src/components/quoteGenerator.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+
+const QuoteGenerator = () => {
+
+    const quotes = [
+        {quote: "The only limit to our realization of tomorrow will be our doubts of today." , author: "Unknown"},
+        {quote: "Be the change that you wish to see in the world." , author: "Mahatma Gandhi"},
+        {quote: "You miss 100% of the shots you never take." , author: "Wayne Gretzky"},
+        {quote: "It's never too late to be what you might have been." , author: "Unknwon"},
+        {quote: "The most common way people give up their power is by thinking they don't have any." , author: "Alice Walker"},
+        {quote: "Don't judge each day by the harvest you reap but by the seeds that you plant." , author: "Robert Louis Stevenson"},
+        {quote: "The only way to do great work is to love what you do." , author: "Steve Jobs"},
+        {quote: "The future belongs to those who believe in the beauty of their dreams." , author: "Eleanor Roosevelt"},
+        {quote: "Do one thing every day that scares you." , author: "Eleanor Roosevelt"},
+        ];
+    
+
+    return(
+        <div className="quote-generator">
+            <p>The quote has to be here!</p>
+            <p>The author has to be here!</p>
+            <button>New quote!</button>
+        </div>
+    );
+}
+
+export default QuoteGenerator;
